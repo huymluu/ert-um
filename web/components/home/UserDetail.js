49aa945dd949cf00a77a452340c1885bece9e78f
Akivamu <akivamu@gmail.com>
2018-09-13 16:37:03 +0700

    UserDetail style

--- web/components/home/UserDetail.js
+++ web/components/home/UserDetail.js
@@ -4,8 +4,28 @@ export default class UserDetail extends React.Component {
 
   render () {
     return (
-      <div >
-        { this.props.user && this.props.user.full_name }
+      <div style={{margin: '30px'}}>
+        { this.props.user &&
+        <div>
+          <div className="row">
+            <label className="col-md-3">ID</label>
+            {this.props.user.id}
+          </div>
+          <div className="row">
+            <label className="col-md-3">Username</label>
+            {this.props.user.username}
+          </div>
+          <div className="row">
+            <label className="col-md-3">Fullname</label>
+            {this.props.user.full_name}
+          </div>
+          <div className="row">
+            <label className="col-md-3">DoB</label>
+            {this.props.user.dob}
+          </div>
+          <button className="btn btn-primary" href="#">Edit</button>
+        </div>
+        }
       </div>
     )
   }
