b5677ccdafb53b297633cec28a508867a7e3d34b
Akivamu <akivamu@gmail.com>
2018-09-14 08:08:02 +0700

    remove fix code

--- server/oauth.js
+++ server/oauth.js
@@ -8,9 +8,9 @@ router.get('/authorized', function (req, res) {
 
   if (!req.query.code) {
     if (req.query.error) {
-      res.redirect('http://localhost:8080/oauth/error?code=' + encodeURIComponent(req.query.error))
+      res.redirect('/oauth/error?code=' + encodeURIComponent(req.query.error))
     } else {
-      res.redirect('http://localhost:8080/oauth/error?message=' + encodeURIComponent('OAuth server return empty code'))
+      res.redirect('/oauth/error?message=' + encodeURIComponent('OAuth server return empty code'))
     }
   } else {
     let postBody = 'grant_type=authorization_code&code=' + req.query.code +
@@ -28,7 +28,7 @@ router.get('/authorized', function (req, res) {
       console.log(body)
       let payload = JSON.parse(body)
       if (payload && payload.access_token) {
-        res.redirect('http://localhost:8080/oauth/token?token=' + payload.access_token)
+        res.redirect('/oauth/token?token=' + payload.access_token)
       } else {
         res.send('Error when exchange code for token')
       }
