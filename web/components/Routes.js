57c86c9444f05883e3cd575f56ccdee8cc2a2161
Akivamu <akivamu@gmail.com>
2018-09-14 08:29:20 +0700

    Remove old components

--- web/components/Routes.js
+++ web/components/Routes.js
@@ -1,34 +1,9 @@
 import React from 'react'
-import { BrowserRouter as Router, Redirect, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 import OAuthTokenView from './oauth/OAuthTokenView'
 import OAuthErrorView from './oauth/OAuthErrorView'
-import Authenticator from './Authenticator'
 import HomeView from './home/HomeView'
 import AddUserView from './home/AddUserView'
-import AdminView from './admin/AdminView'
-
-const PrivateRoute = ({component, ...rest}) => (
-  <Route {...rest} render={ function (props) {
-    if (!Authenticator.isLoggedIn()) {
-      return (
-        <Redirect to={{
-          pathname: '/',
-          state: {from: props.location}
-        }}/>
-      )
-    }
-
-    if (!Authenticator.isAuthorized(props.location.pathname)) {
-      return (
-        <div>
-          <h1>Access denied</h1>
-        </div>
-      )
-    }
-
-    return React.createElement(component, props)
-  }}/>
-)
 
 export default (
   <div>
@@ -38,7 +13,6 @@ export default (
         <Route exact path="/adduser" component={AddUserView}/>
         <Route exact path="/oauth/token" component={OAuthTokenView}/>
         <Route exact path="/oauth/error" component={OAuthErrorView}/>
-        <PrivateRoute path="/admin" component={AdminView}/>
       </div>
     </Router>
   </div>
