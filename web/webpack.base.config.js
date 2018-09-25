524bcf29ee73806aecb2447cc798ec2fd001b27f
Akivamu <akivamu@gmail.com>
2018-09-25 08:57:09 +0700

    Use npm module bootstrap

--- web/webpack.base.config.js
+++ web/webpack.base.config.js
@@ -23,15 +23,7 @@ module.exports = {
       },
       {
         test: /\.css$/,
-        use: [
-          {loader: 'style-loader'},
-          {
-            loader: 'css-loader',
-            options: {
-              modules: true
-            }
-          }
-        ]
+        use: ['style-loader', 'css-loader']
       }
     ]
   },
