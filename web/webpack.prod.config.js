a5207ee9592a5e3ba99dee4cfc11f532a1ac2f23
Akivamu <akivamu@gmail.com>
2018-09-25 09:32:25 +0700

    Webpack bundle analyzer

--- web/webpack.prod.config.js
+++ web/webpack.prod.config.js
@@ -1,6 +1,14 @@
 const merge = require('webpack-merge')
 const baseConfig = require('./webpack.base.config.js')
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin
 
 module.exports = merge(baseConfig, {
-  mode: 'production'
+  mode: 'production',
+  plugins: [
+    new BundleAnalyzerPlugin({
+      analyzerMode: 'static',
+      openAnalyzer: false,
+      reportFilename: '../bundle-analyzer-report.html'
+    })
+  ]
 })
